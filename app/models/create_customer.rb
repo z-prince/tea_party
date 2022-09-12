41d9f5c84b950104bcca0f038973bab60ef7cc2b
Zach Prince <zachary.s.prince@outlook.com>
2022-09-12 15:46:26 -0400

    Remove test and fix migration

--- app/models/create_customer.rb
+++
@@ -1,2 +0,0 @@
-class CreateCustomer < ApplicationRecord
-end
--- db/migrate/20220912194603_create_customers.rb
+++ db/migrate/20220912194603_create_customers.rb
@@ -1,6 +1,6 @@
-class CreateCreateCustomers < ActiveRecord::Migration[5.2]
+class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
-    create_table :create_customers do |t|
+    create_table :customers do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
