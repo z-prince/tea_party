7199b3f8bdd64cae5df36af8e70e94ef5691ad00
Zach Prince <zachary.s.prince@outlook.com>
2022-09-12 15:43:50 -0400

    Add customer migration

---
+++ db/migrate/20220912194309_create_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :create_customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
