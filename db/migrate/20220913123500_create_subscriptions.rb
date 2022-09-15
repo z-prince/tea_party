f5c2d128e0b5da30c8c6fabe20509cf87012fe59
Zach Prince <zachary.s.prince@outlook.com>
2022-09-15 09:38:22 -0400

    Fix titel typo

--- db/migrate/20220913123500_create_subscriptions.rb
+++ db/migrate/20220913123500_create_subscriptions.rb
@@ -1,7 +1,7 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
-      t.string :titel
+      t.string :title
       t.integer :price
       t.boolean :status
       t.integer :frequency
