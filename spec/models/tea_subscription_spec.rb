dba14ca66a7f1ad2768615d5f9f7f61fcbfbecb9
Zach Prince <zachary.s.prince@outlook.com>
2022-09-15 09:51:18 -0400

    Add tea_subscription tests

---
+++ spec/models/tea_subscription_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe TeaSubscription, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:tea) }
+    it { should belong_to(:subscription) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :tea_id }
+    it { should validate_presence_of :subscription_id }
+  end
+end
